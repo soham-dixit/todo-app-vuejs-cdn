f9dcd8e32e5d6272d4dbd330664a5da575c48886
sohamdixit <sohamddixit@gmail.com>
2021-08-17 13:54:40 +0530

    v4

--- js/main.js
+++ js/main.js
@@ -1,29 +1,27 @@
- new Vue({
-            el: '#app',
-            
-            data: {
-                isEditing: false,
-                todo: '',
-                todos: [],
-                selectedTodo: null
-            },
-            
-            methods: {
-                storeTodo() {
-                    if(this.todo!=""){
-                        this.todos.push(this.todo)
-                        this.todo = ''
-                    }
-                },
+Vue.config.devtools = true;
+new Vue({
+  el: "#app",
 
-                removeTodo(index) {
-                    this.todos.splice(index, 1)
-                },
-                
-                // editTodo(index, todo) {
-                //     this.isEditing = true
-                //     this.todo = todo
-                //     this.selectedIndex = index
-                // }
-            }
-        })
+  data: {
+    todo: "",
+    todos: [],
+    selectedTodo: null,
+  },
+
+  methods: {
+    storeTodo() {
+      if (this.todo != "") {
+        this.todos.push({name: this.todo, isStrikedOff: false});
+        this.todo = "";
+      }
+    },
+
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    },
+
+    doneTodo(index){
+        this.todos[index].isStrikedOff=true;
+    },
+  },
+});
